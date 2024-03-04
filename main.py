2006ad92d19c04af80147f9ab12dd21b5db02fd1
NeoJoker26 <71454749+NeoJoker26@users.noreply.github.com>
2024-03-04 04:42:56 +0000

    Tkinter is fixed, loads csv and excel, sends file to database handler

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import tkinter as tk
-from tkinter import ttk, filedialog, messagebox
+from tkinter import ttk, filedialog
 import pandas as pd
+import chardet
+import pyarrow as pa
 import seaborn as sns
 import os
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-import sqlalchemy as sa
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+import csv
+
+
+class GraphTheory:
+    def __init__(self):
+        pass
+
+    def visualize_square_feet_vs_price(self):
+        sns.scatterplot(x='SquareFeet', y='Price', data=self.df)
 
 
 class PredictionAlgorithm:
@@ -43,174 +54,150 @@ class PredictionAlgorithm:
         print(predictions)
 
 
-class SecurityCheck:
+class EnchanceSecurity:
     def __int__(self):
         pass
 
 
 class DatabaseHandler:
     def __init__(self):
-        postgres_url = 'postgresql://neojoker:password123@neojoker/mockdb.db'
-        # Create the engine to connect to the database
-        engine = sa.create_engine('sqlite:///houses.db')
+        # Adjust the connection details here
+        self.engine = create_engine('postgresql://neojoker26:password123@localhost/dissydb')
 
-        # Create the session maker
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
+    def create_table(self, table_name, column_names):
+        try:
+            table = Table(table_name, *[Column(col_name, String()) for col_name in column_names])
+            table.create(self.engine)
+            print("Table created successfully.")
+        except Exception as e:
+            print("Error creating table:", e)
 
-    def start_database(self):
-        engine = create_engine
-        pass
-
-
-class GraphTheory:
-    def __init__(self):
-        pass
-
-    def visualize_square_feet_vs_price(self):
-        # Create a scatter plot of square feet vs. price
-        sns.scatterplot(x='SquareFeet', y='Price', data=self.df)
-
-    def visualize_bedrooms_vs_price(self):
-        # Create a scatter plot of bedrooms vs. price
-        sns.scatterplot(x='Bedrooms', y='Price', data=self.df)
-
-    def visualize_bathrooms_vs_price(self):
-        # Create a scatter plot of bathrooms vs. price
-        sns.scatterplot(x='Bathrooms', y='Price', data=self.df)
-
-    def visualize_neighborhood_vs_price(self):
-        # Create a bar plot of average price per neighborhood
-        sns.barplot(x='Neighborhood', y='Price', data=self.df, estimator=np.mean)
-
-    def visualize_year_built_vs_price(self):
-        # Create a box plot of price vs. year built
-        sns.boxplot(x='YearBuilt', y='Price', data=self.df)
+    def insert_data(self, table_name, df):
+        try:
+            conn = self.engine.connect()
+            df.to_sql(table_name, conn, if_exists='replace', index=False)
+            print("Data inserted into table successfully.")
+        except Exception as e:
+            print("Error inserting data into table:", e)
 
 
 class WindowMaker:
     def __init__(self):
-        pd.set_option('display.max_column', None)  # max number of columns that pandas can pull up
-        pd.set_option('display.max_rows', None)  # max number of rows that pandas can pull up
-        pd.set_option('display.max_seq_items', None)  # removes the limit on the number of items that can be displayed
-        pd.set_option('display.max_colwidth', 500)  # sets the max pandas column width to 500 chars
-        pd.set_option('expand_frame_repr', True)  # ensures that the DF expands over a few lines
-        pd.set_option('display.precision', 10)  # sets floats to 10 dp
-        pd.set_option('display.colheader_justify', 'left')  # sets column headers to left
+        # Configure pandas display options
+        pd.set_option('display.max_column', None)
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_seq_items', None)
+        pd.set_option('display.max_colwidth', 500)
+        pd.set_option('expand_frame_repr', True)
+        pd.set_option('display.precision', 10)
+        pd.set_option('display.colheader_justify', 'left')
 
-        self.window = tk.Tk()  # creates main window called window
-        self.window.title("Search for Database")  # title of the tkinter window
-        self.window.geometry("800x600")  # the size of the tkinter window
-        self.window.minsize(800, 600)  # sets the min size of the tkinter window
+        self.window = tk.Tk()
+        self.window.title("Search for Database")
+        self.window.geometry("800x600")
+        self.window.minsize(800, 600)
 
-        self.file_path = None  # file path for file
-        self.df = None  # makes an empty dataframe until it can be used amongst the class
-        self.file_type = ""  # tells file type for display_data method
+        # Create variables for file path and data
+        self.file_path = ""
+        self.data = None
 
-        self.excel_frame = tk.LabelFrame(self.window, text="Excel Database")  # title for Labelframe for  treeview
-        self.excel_frame.pack(fill="both", expand=True)  #
+        # Create variable for sheets
+        self.sheet_names = None
+        self.sheet_data = {}
 
-        self.excel_treeview = ttk.Treeview(self.excel_frame, columns=(), show="headings")  # creates treeview to view db
-        self.excel_treeview.pack(expand=True, fill="both")
+        # Create the main window widgets
+        self.button_frame = tk.Frame(self.window)
+        self.button_frame.pack()
 
-        # creates the x and y scroll bars, put into frame not treeview for aesthetics
-        self.y_scroll = tk.Scrollbar(self.excel_frame, orient="vertical", command=self.excel_treeview.yview)
-        self.x_scroll = tk.Scrollbar(self.excel_frame, orient="horizontal", command=self.excel_treeview.xview)
-        self.y_scroll.pack(side="right", fill="y")
-        self.x_scroll.pack(side="bottom", fill="x")
-        self.excel_treeview.config(yscrollcommand=self.y_scroll.set, xscrollcommand=self.x_scroll.set)
+        self.open_button = tk.Button(self.button_frame, text="Open File", command=self.open_file)
+        self.open_button.pack(side=tk.LEFT, padx=5)
 
-        # creates open file title for frame for button
-        self.open_frame = tk.LabelFrame(self.window, text="Open File", labelanchor='n')
-        self.open_frame.pack(fill="both", expand=True)
+        self.send_to_db_button = tk.Button(self.button_frame, text="Send Through Database")
+        self.send_to_db_button.pack(side=tk.LEFT, padx=5)
 
-        # creates add db button
-        self.add_database_button = tk.Button(self.open_frame, text="Add database", command=self.open_file)
-        self.add_database_button.pack(expand=True)
+        self.label_filename = tk.Label(self.window, text="", wraplength=300)
+        self.label_filename.pack()
 
-        # title for chosen file
-        self.database_label = tk.Label(self.open_frame, text="FileNaN")
-        self.database_label.pack(expand=True)
+        # Create vertical scrollbar
+        self.y_scrollbar = ttk.Scrollbar(self.window, orient=tk.VERTICAL)
+        self.y_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        self.selected_file_path = None
+        # Create horizontal scrollbar
+        self.x_scrollbar = ttk.Scrollbar(self.window, orient=tk.HORIZONTAL)
+        self.x_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+
+        # Create a text box to display the file content
+        self.text_box = tk.Text(self.window, wrap=tk.NONE, yscrollcommand=self.y_scrollbar.set,
+                                xscrollcommand=self.x_scrollbar.set)
+        self.text_box.pack(fill=tk.BOTH, expand=True)
+        self.y_scrollbar.config(command=self.text_box.yview)
+        self.x_scrollbar.config(command=self.text_box.xview)
+
+        # Create a listbox to display the detected sheets
+        self.sheet_listbox = tk.Listbox(self.window, selectmode=tk.SINGLE, yscrollcommand=self.y_scrollbar.set)
+        self.sheet_listbox.pack(fill=tk.BOTH, expand=True)
+
+        # Bind double-click event on the listbox to select the sheet
+        self.sheet_listbox.bind('<Double-Button-1>', self.select_sheet)
+
+        # Send file to database class
+        self.handle_db = DatabaseHandler()
 
     def main(self):
-        self.window.mainloop()  # calls tkinter window
-        handler = DatabaseHandler()
-        return self.selected_file_path
+        self.window.mainloop()
 
     def open_file(self):
-        file_types = (
-            ("Excel Files", "*.xlsx"),
-            ("Excel Files", "*.xls"),
+        self.file_path = filedialog.askopenfilename(filetypes=[
             ("CSV Files", "*.csv"),
+            ("Excel Files", "*.xlsx *.xls"),
             ("All Files", "*.*")
-        )
+        ])
+        if self.file_path and self.file_path.endswith(".csv"):
+            self.parse_csv()
+        elif self.file_path.endswith((".xlsx", ".xls")):
+            self.parse_excel(self.file_path)
 
-        self.file_path = filedialog.askopenfilename(title="Open File", filetypes=file_types)
+    def parse_csv(self):
+        try:
+            self.text_box.delete('1.0', tk.END)
+            with open(self.file_path, 'rb') as f:
+                rawdata = f.read()
+            encoding = chardet.detect(rawdata)['encoding']
+            df = pd.read_csv(self.file_path, encoding=encoding)
+            self.text_box.insert(tk.END, df.to_string(index=False))
+            self.data = df
+            self.handle_db.create_table("csv_data", df.columns.tolist())
+            self.handle_db.insert_data("csv_data", df)
+        except Exception as e:
+            print("Error:", e)
 
-        if self.file_path:
-            file_name = self.file_path
-            self.database_label["text"] = os.path.basename(file_name)
+    def parse_excel(self, file_path):
+        try:
+            self.text_box.delete('1.0', tk.END)
+            df = pd.read_excel(file_path)
+            self.text_box.insert(tk.END, df.to_string(index=False))
+            self.sheet_listbox.delete(0, tk.END)
+            for sheet_name in pd.ExcelFile(file_path).sheet_names:
+                self.sheet_listbox.insert(tk.END, sheet_name)
+            self.data = df
+            self.handle_db.create_table("excel_data", df.columns.tolist())
+            self.handle_db.insert_data("excel_data", df)
+        except Exception as e:
+            print("Error parsing Excel file:", e)
 
-            if file_name.endswith(".csv"):
-                encodings = ['ISO-8859-1', 'utf-8']
-                for encoding in encodings:
-                    try:
-                        self.df = pd.read_csv(self.file_path, encoding=encoding)
-                        self.file_type = "csv"  # Set self.file_type directly
-                        break
-                    except FileNotFoundError:
-                        messagebox.showerror("File Not Found", "The file is either not supported or non-existent")
-            elif file_name.endswith((".xlsx", ".xls")):
-                try:
-                    self.df = pd.read_excel(self.file_path)
-                    self.file_type = "excel"  # Set self.file_type directly
-                except FileNotFoundError:
-                    messagebox.showerror("File Not Found", "The file is either not supported or non-existent")
-
-            if self.file_type:
-                self.display_data(self.df, self.file_type)
-            else:
-                messagebox.showerror("Unsupported File Type", "The selected file is not supported.")
-        else:
-            messagebox.showerror("Unsupported File Type", "The selected file is not supported.")
-        self.selected_file_path = self.file_path
-
-    def display_data(self, df, filetype, sheet_name=None):
-        # Clear the existing columns in the Treeview
-        for column in self.excel_treeview.get_children():
-            self.excel_treeview.delete(column)
-
-        column_names = df.columns.tolist()  # Get column names
-
-        # Set columns in Treeview
-        self.excel_treeview["columns"] = column_names
-
-        # Configure column headings and set their width
-        for column in column_names:
-            self.excel_treeview.heading(column, text=column)
-            self.excel_treeview.column(column, width=150, minwidth=100)
-
-        if filetype == "csv":
-            # Insert data for a CSV file
-            for index, row in df.iterrows():
-                self.excel_treeview.insert("", "end", values=tuple(row))
-        elif filetype == "excel":
-            if sheet_name:
-                # Check if the specified sheet_name exists in the Excel file
-                if sheet_name in df.sheet_names:
-                    selected_sheet = df.parse(sheet_name)
-                    for index, row in selected_sheet.iterrows():
-                        self.excel_treeview.insert("", "end", values=tuple(row))
-                else:
-                    messagebox.showerror("Sheet Not Found", "The selected sheet is not in the Excel file.")
-            else:
-                messagebox.showerror("Sheet Name Required", "You must provide a sheet name for Excel files.")
+    def select_sheet(self, event):
+        selected_index = self.sheet_listbox.curselection()
+        if selected_index:
+            selected_sheet = self.sheet_listbox.get(selected_index)
+            excel_file = pd.ExcelFile(self.file_path)
+            try:
+                df = excel_file.parse(selected_sheet)
+                self.text_box.delete('1.0', tk.END)
+                self.text_box.insert(tk.END, df.to_string(index=False))
+            except Exception as e:
+                print(f"Error parsing sheet '{selected_sheet}':", e)
 
 
-if __name__ == '__main__':
-    window_maker = WindowMaker()
-    selected_file = window_maker.main()
-    if selected_file:
-        predictor = PredictionAlgorithm(selected_file)
+if __name__ == "__main__":
+    window = WindowMaker()
+    window.main()
